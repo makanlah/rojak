4d1820ac0246bd96a94c15b1b07099774f6c21d0
BinHong Lee <binhonglee@users.noreply.github.com>
2019-04-22 20:58:18 -0700

    Added CORS functionality (#3)
    Added CORS functionality and fixed some runtime errors

--- foodRequest.py
+++ foodRequest.py
@@ -1,24 +1,28 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request
 from flask import abort
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 from process_datasets import *
 
 app = Flask(__name__)
 
 #read from the model
-name_obj, uuid_dish = read_model()
+name_obj, uuid_dish, bigram_dish = read_model()
 
 @app.route('/todo/api/v1.0/recipes/', methods=['POST'])
+@cross_origin()
 def get_data():
-
     if "content" in request.json:
-        name = request.json['content']
+        name = json.loads(request.json)["content"]
     else:
         print("Error! No content found!")
         abort(404)
 
     #function from process_datasets
-    task = predict(name, name_obj, uuid_dish)
+    task = predict(name, name_obj, uuid_dish, bigram_dish)
     if len(task) == 0:
         abort(404)
     return jsonify(task)
